5e6e4f3b579fb1d61b7c96c7970abd3077032d32
Pratyush Kurhe <pratyush.kurhe@gmail.com>
2023-01-29 19:41:44 +0530

    new changes added

---
+++ prac2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class prac2{
+public static void main(String arg[]){
+    System.out.println("Hello prac 2 ");
+
+
+}
+
+}
---
+++ prac3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class prac3{
+public static void main(String arg[]){
+    System.out.println("Hello prac3 ");
+
+
+}
+
+}
